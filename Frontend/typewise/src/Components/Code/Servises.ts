31d103a97a9ec2e14f0eb8b174e172b8eec68530
NeryJim21 <2997668450101@ingenieria.usac.edu.gt>
2023-04-28 16:21:44 -0600

    update: unión commit anterior

--- Frontend/typewise/src/Components/Code/Servises.ts
+++ Frontend/typewise/src/Components/Code/Servises.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const path = 'http://localhost:4000/'
+const path = 'http://localhost:3000/'
 
 export const getOutput = async(code:string) => {
     return await axios.post(path, {code: code})
--- Frontend/typewise/src/Components/Table/Services.ts
+++ Frontend/typewise/src/Components/Table/Services.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const path = 'http://localhost:4000/'
+const path = 'http://localhost:3000/'
 
 export const getTokens = async() => {
     return await axios.get(path  + 'Tokens')
